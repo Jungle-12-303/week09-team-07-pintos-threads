74996c529013ba6625cbf43d16c8cfe191bf82e9
Donghyun Kim <kdh949@naver.com>
2026-04-29 20:42:34 +0900

    Update pintos/threads/thread.c

--- pintos/threads/thread.c
+++ pintos/threads/thread.c
@@ -371,7 +371,7 @@ thread_set_priority (int new_priority) {
 		return;
 	}
 
-	t = list_entry (first_node, struct thread, elem); // 그 list_elem을 포함하는 thread를 꺼낸다.
+	t = list_entry (list_front(&ready_list), struct thread, elem); // 그 list_elem을 포함하는 thread를 꺼낸다.
 
 	// ready_list에 현재 스레드보다 더 높은 priority 스레드가 있는지 확인한다.
 	if ( t->priority > thread_current ()->priority) {
